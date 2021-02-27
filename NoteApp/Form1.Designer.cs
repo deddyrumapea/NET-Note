d0eb72b6d106919e00cbac08db82835f4a6bc219
Deddy Romnan <romnandeddy@gmail.com>
2021-02-27 21:01:35 +0700

    feat: add user interface

--- NoteApp/Form1.Designer.cs
+++ NoteApp/Form1.Designer.cs
@@ -29,128 +29,157 @@ namespace NoteApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonDecreaseFontSize = new System.Windows.Forms.Button();
+            this.comboBoxFontSize = new System.Windows.Forms.ComboBox();
+            this.buttonIncreaseFontSize = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.buttonStrikeout = new System.Windows.Forms.Button();
+            this.buttonUnderline = new System.Windows.Forms.Button();
+            this.buttonItalic = new System.Windows.Forms.Button();
+            this.buttonBold = new System.Windows.Forms.Button();
+            this.richTextBoxNote = new System.Windows.Forms.RichTextBox();
+            this.comboBoxTextAlignment = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonDecreaseFontSize
             // 
-            this.button1.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(13, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(35, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "-";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonDecreaseFontSize.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecreaseFontSize.Location = new System.Drawing.Point(13, 33);
+            this.buttonDecreaseFontSize.Name = "buttonDecreaseFontSize";
+            this.buttonDecreaseFontSize.Size = new System.Drawing.Size(35, 35);
+            this.buttonDecreaseFontSize.TabIndex = 0;
+            this.buttonDecreaseFontSize.Text = "-";
+            this.buttonDecreaseFontSize.UseVisualStyleBackColor = true;
+            this.buttonDecreaseFontSize.Click += new System.EventHandler(this.buttonDecreaseFontSize_Click);
             // 
-            // comboBox1
+            // comboBoxFontSize
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(49, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(86, 28);
-            this.comboBox1.TabIndex = 1;
+            this.comboBoxFontSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFontSize.FormattingEnabled = true;
+            this.comboBoxFontSize.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "18",
+            "24",
+            "30",
+            "36",
+            "48",
+            "60",
+            "72",
+            "96"});
+            this.comboBoxFontSize.Location = new System.Drawing.Point(51, 34);
+            this.comboBoxFontSize.Name = "comboBoxFontSize";
+            this.comboBoxFontSize.Size = new System.Drawing.Size(69, 28);
+            this.comboBoxFontSize.TabIndex = 1;
+            this.comboBoxFontSize.SelectedIndexChanged += new System.EventHandler(this.comboBoxFontSize_SelectedIndexChanged);
             // 
-            // button2
+            // buttonIncreaseFontSize
             // 
-            this.button2.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(141, 36);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(35, 35);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "+";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonIncreaseFontSize.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIncreaseFontSize.Location = new System.Drawing.Point(124, 33);
+            this.buttonIncreaseFontSize.Name = "buttonIncreaseFontSize";
+            this.buttonIncreaseFontSize.Size = new System.Drawing.Size(35, 35);
+            this.buttonIncreaseFontSize.TabIndex = 2;
+            this.buttonIncreaseFontSize.Text = "+";
+            this.buttonIncreaseFontSize.UseVisualStyleBackColor = true;
+            this.buttonIncreaseFontSize.Click += new System.EventHandler(this.buttonIncreaseFontSize_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button7);
-            this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBoxTextAlignment);
+            this.groupBox1.Controls.Add(this.buttonStrikeout);
+            this.groupBox1.Controls.Add(this.buttonUnderline);
+            this.groupBox1.Controls.Add(this.buttonItalic);
+            this.groupBox1.Controls.Add(this.buttonBold);
+            this.groupBox1.Controls.Add(this.buttonDecreaseFontSize);
+            this.groupBox1.Controls.Add(this.buttonIncreaseFontSize);
+            this.groupBox1.Controls.Add(this.comboBoxFontSize);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(776, 85);
+            this.groupBox1.Size = new System.Drawing.Size(1114, 85);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Text Appearance";
             // 
-            // button5
+            // buttonStrikeout
             // 
-            this.button5.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(290, 33);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(35, 35);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "U";
-            this.button5.UseVisualStyleBackColor = true;
+            this.buttonStrikeout.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStrikeout.Location = new System.Drawing.Point(330, 33);
+            this.buttonStrikeout.Name = "buttonStrikeout";
+            this.buttonStrikeout.Size = new System.Drawing.Size(35, 35);
+            this.buttonStrikeout.TabIndex = 8;
+            this.buttonStrikeout.Text = "S";
+            this.buttonStrikeout.UseVisualStyleBackColor = true;
+            this.buttonStrikeout.Click += new System.EventHandler(this.buttonStrikeout_Click);
             // 
-            // button4
+            // buttonUnderline
             // 
-            this.button4.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(250, 33);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(35, 35);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "I";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonUnderline.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUnderline.Location = new System.Drawing.Point(290, 33);
+            this.buttonUnderline.Name = "buttonUnderline";
+            this.buttonUnderline.Size = new System.Drawing.Size(35, 35);
+            this.buttonUnderline.TabIndex = 7;
+            this.buttonUnderline.Text = "U";
+            this.buttonUnderline.UseVisualStyleBackColor = true;
+            this.buttonUnderline.Click += new System.EventHandler(this.buttonUnderline_Click);
             // 
-            // button3
+            // buttonItalic
             // 
-            this.button3.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(210, 33);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(35, 35);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "B";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonItalic.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonItalic.Location = new System.Drawing.Point(250, 33);
+            this.buttonItalic.Name = "buttonItalic";
+            this.buttonItalic.Size = new System.Drawing.Size(35, 35);
+            this.buttonItalic.TabIndex = 6;
+            this.buttonItalic.Text = "I";
+            this.buttonItalic.UseVisualStyleBackColor = true;
+            this.buttonItalic.Click += new System.EventHandler(this.buttonItalic_Click);
             // 
-            // richTextBox1
+            // buttonBold
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 118);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 457);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
+            this.buttonBold.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBold.Location = new System.Drawing.Point(210, 33);
+            this.buttonBold.Name = "buttonBold";
+            this.buttonBold.Size = new System.Drawing.Size(35, 35);
+            this.buttonBold.TabIndex = 5;
+            this.buttonBold.Text = "B";
+            this.buttonBold.UseVisualStyleBackColor = true;
+            this.buttonBold.Click += new System.EventHandler(this.buttonBold_Click);
             // 
-            // button6
+            // richTextBoxNote
             // 
-            this.button6.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(330, 33);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(35, 35);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "S";
-            this.button6.UseVisualStyleBackColor = true;
+            this.richTextBoxNote.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxNote.Location = new System.Drawing.Point(12, 118);
+            this.richTextBoxNote.Name = "richTextBoxNote";
+            this.richTextBoxNote.Size = new System.Drawing.Size(1114, 549);
+            this.richTextBoxNote.TabIndex = 4;
+            this.richTextBoxNote.Text = "";
             // 
-            // button7
+            // comboBoxTextAlignment
             // 
-            this.button7.Location = new System.Drawing.Point(440, 33);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(70, 35);
-            this.button7.TabIndex = 9;
-            this.button7.Text = "Center";
-            this.button7.UseVisualStyleBackColor = true;
+            this.comboBoxTextAlignment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTextAlignment.Items.AddRange(new object[] {
+            "Align Left",
+            "Align Center",
+            "Align Right"});
+            this.comboBoxTextAlignment.Location = new System.Drawing.Point(469, 34);
+            this.comboBoxTextAlignment.Name = "comboBoxTextAlignment";
+            this.comboBoxTextAlignment.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxTextAlignment.TabIndex = 0;
+            this.comboBoxTextAlignment.SelectedIndexChanged += new System.EventHandler(this.comboBoxTextAlignment_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 587);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1138, 679);
+            this.Controls.Add(this.richTextBoxNote);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Note App";
             this.groupBox1.ResumeLayout(false);
@@ -160,16 +189,16 @@ namespace NoteApp
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonDecreaseFontSize;
+        private System.Windows.Forms.ComboBox comboBoxFontSize;
+        private System.Windows.Forms.Button buttonIncreaseFontSize;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button buttonUnderline;
+        private System.Windows.Forms.Button buttonItalic;
+        private System.Windows.Forms.Button buttonBold;
+        private System.Windows.Forms.RichTextBox richTextBoxNote;
+        private System.Windows.Forms.Button buttonStrikeout;
+        private System.Windows.Forms.ComboBox comboBoxTextAlignment;
     }
 }
 
